7fafd8e1d0a14eac141733d3c6c650010cbe43be
andrew-oko-odion <mrniceguy4net@yahoo.com>
2017-01-28 08:27:35 +0100

    Add dashboad for employers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource)
+    resource.is_a?(Employer) ? dashboard_index_path : root_path
+  end
 
   def home
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+
+  get 'dashboard/index'
+
+  root 'welcome#index'
+  
+  #get 'welcome/index'
+
+  #get 'welcome/show'
+
+  #get 'welcome/apply'
+
   devise_for :employers
   devise_for :jobseekers
-  root 'application#home'
 end
