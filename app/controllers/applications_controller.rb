cd445209c21cdd1e1ab64cf537077239497836e8
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-03-15 06:05:43 -0400

    Change Job post to Applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,9 +1,15 @@
 
+# Handle Applications for the jobseeker while it also handles Employers selection of candidate
 class ApplicationsController < ApplicationController
+  layout 'dashboard'
   def index
+    # List of submited applications for a particular Job.
+    @all = Application.where(:employer_id = current_employer.id)
   end
 
   def show
+    # shows a Jobseekers profile I have not decided whether it should be a modal or not 
+    # @profile = @application.find("where: Job.employer_id.jobseeker.id")
   end
 
   def new
@@ -11,7 +17,6 @@ class ApplicationsController < ApplicationController
   end
 
   def create
- 
     if @applications = Application.new(applications_params) &&  current_jobseeker 
       @applications.save
       flash[:notice] = 'Application sent Successfully'
@@ -27,5 +32,8 @@ class ApplicationsController < ApplicationController
   def applications_params
     params.require(:applications).permit(:jobseeker_id, :job_id)
   end
-  
+
+  def job_params
+    params.require(:job).permit(:title, :job_type_id, :job_category_id, :description, :application_email, :location, :company_name, :closing_date, :salary, :employer_id)
+  end
 end
