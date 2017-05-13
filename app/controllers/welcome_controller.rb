614c7362c9b14e7c0bef4d9bfd0246db39af79c8
andrew oko-odion <andrewokoodion@gmail.com>
2017-05-13 13:37:48 +0100

    Replace Pagination from will_paginate to kaminari

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,33 @@
-
 class  WelcomeController < ApplicationController
+# load_and_authorize_resource :only => [:index, :show] 
   layout 'welcome_layout'
   attr_accessor :jobs_cart
+  #check_authorization
+  #load_and_authorize_resource 
+  
+  def autocomplete
+    render json: Job.search(params[:query], autocomplete: true, limit: 10).map  do |j|
+      { title: j.title, value: j.id }
+    end
+  end
   
   def index
+    if params[:query].present?
+      @jobs = Job.search(params[:query], page: params[:page]) #where(job_type: 1, hide: false).paginate(:page => params[:page], :per_page => 6)
+    else
+      # @books = Book.all.page params[:page]
+      # @jobs = Job.where(job_type: 1, hide: false).paginate(:page => params[:page], :per_page => 6)
+      # @internships = Job.where(job_type: 2, hide: false).paginate(:page => params[:page], :per_page => 6)
+      @jobs = Job.where(job_type: 1, hide: false).order('id DESC').page params[:page]
+      @internships = Job.where(job_type: 2, hide: false).order('id DESC').page params[:page]
+    end
+    
     # lookup the list for Job_type ...
     # table job_type id of 1 is for `open jobs`
     # while Table job_type of id 2 is for `paid internships`
-    @jobs = Job.where(job_type: 1, hide: false).paginate(:page => params[:page], :per_page => 6)
-    @internships = Job.where(job_type: 2, hide: false).paginate(:page => params[:page], :per_page => 6)
+    # @jobs = Job.where(job_type: 1, hide: false).paginate(:page => params[:page], :per_page => 6)
+    # @internships = Job.where(job_type: 2, hide: false).paginate(:page => params[:page], :per_page => 6)
+    # authorize! :read, @jobs
   end
 
   def internship
@@ -26,23 +45,35 @@ class  WelcomeController < ApplicationController
   end
 
   def create
-    if jobseeker_signed_in? 
-      @apply = Application.new(applications_params) #&& current_jobseeker 
-      if @apply.save
-        flash[:notice] = 'Application sent Successfully'
-        redirect_to welcome_index_path
+    @apply = Application.new(applications_params)
+    
+    respond_to do |format|
+      if jobseeker_signed_in? 
+        if @apply.save
+          format.html { redirect_to welcome_index_path, notice: 'Application sent Successfully.' }
+          format.js
+          # format.json { render json: @user, status: :created, location: @user }
+        end
+      else
+        format.html { redirect_to new_jobseeker_session_path, error: 'Please Login to apply' }
+        # format.json { render json: @user.errors, status: :unprocessable_entity }
       end
-    else
-      #render plain: session[:applying]
-      flash[:error] = 'Please Login to apply'
-      redirect_to new_jobseeker_session_path 
     end
   end
-
   
-  def set_cart_to_array
-    cool[:cart_items] = []
-  end 
+  #   if jobseeker_signed_in? 
+  #     @apply = Application.new(applications_params) 
+  #     if @apply.save
+  #       flash[:notice] = 'Application sent Successfully'
+  #       redirect_to welcome_index_path
+  #     end
+  #   else
+  #     #render plain: session[:applying]
+  #     flash[:error] = 'Please Login to apply'
+  #     redirect_to new_jobseeker_session_path 
+  #   end
+  # end
+
   
   private
   def job_params
@@ -50,7 +81,7 @@ class  WelcomeController < ApplicationController
   end
 
   def applications_params
-    params.require(:apply).permit(:jobseeker_id, :job_id)
+    params.require(:apply).permit(:jobseeker_id, :job_id, :employer_id, :sort_order_id)
   end
   
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,12 @@
 class Job < ApplicationRecord
+  include Active
+  #  scope :active_jobs, -> { where(active: :false) }
+  paginates_per 10
+  searchkick word_start:  [:title]
   belongs_to :job_type
   belongs_to :job_category
   belongs_to :employer
   has_many :application
+  has_many :payment
+  #belongs_to :application
 end
