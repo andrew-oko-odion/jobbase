ff09cf26a14749c8a298ebfffe52fa6301f6bb39
andrew oko-odion <andrewokoodion@gmail.com>
2017-05-28 16:17:09 +0100

    Add Job nexting from show page
    It allows jobseekers to be able to view the next job without having to
    return to the index page.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,18 +1,34 @@
+
 class Job < ApplicationRecord
-  include Active
-  extend FriendlyId
-  friendly_id :slug_candidates, use: :slugged
-  #  scope :active_jobs, -> { where(active: :false) }
+  extend FriendlyId  
+  friendly_id :slug_jobs, use: :slugged
   searchkick word_start:  [:title]
   belongs_to :job_type
   belongs_to :job_category
   belongs_to :employer
   has_many :application
   has_many :payment
-  #belongs_to :application
-  # paginates_per  8
 
-  def slug_candidates
+ # scope :next, lambda {|id| where("id > ?",id).order("id ASC") }
+ # scope :previous, lambda {|id| where("id < ?",id).order("id DESC") }
+
+  # scope :next, -> { where(job_type: 1, hide: false).where("id > ?", id).order("id ASC") }
+  
+  # def next
+  #   # Job.where(job_type: 1, hide: false).where("id > ?", id).order("id DESC").first || Job.first
+  #   Job.where(job_type: 1, hide: false).where("id > ?", id).order("id ASC").first
+  # end
+
+  def next(job)
+    Job.where(job_type: 1, hide: false).where('id < ?', job.id).last
+  end
+
+  def prev(job)
+    Job.where(job_type: 1, hide: false).where('id > ?', job.id).first
+  end
+
+  
+  def slug_jobs
     [
       :title,
       [:title, :id]
