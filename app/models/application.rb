6a94a16d811af38e3bec8a2970373400d2347055
andrew oko-odion <andrewokoodion@gmail.com>
2017-05-28 16:23:50 +0100

    Prevent jobseeker from multiple applications
     By creating a unique index on two index on the Application Model
     I was able to prevent Jobseekser from muliple applications on a
     specific job. This will eliminate the multiple applications on the
     Applications Table in the Database.

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,7 @@
 class Application < ApplicationRecord
+  validates :id, uniqueness: { scope: [:job_id, :jobseeker_id], message: "you already applied for this job" }, on: :create
   belongs_to :jobseeker
   belongs_to :job
-
-  #scope :profile -> { where () }
+  belongs_to :sort_order
+  belongs_to :employer
 end
