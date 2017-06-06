e74dd8cac5065ea9fe276f5c74b4d2e0a0acf971
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-06-06 16:03:34 +0100

    Cleanup Comment blocks form Source

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,8 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # after_action :store_location
+  layout :layout_by_resource
   
-  # saves the location before loading each page so we can return to the
-  # right page. If we're on a devise page, we don't want to store that as the
-  # place to return to (for example, we don't want to return to the sign in page
-  # after signing in), which is what the :unless prevents
-  # before_action :store_current_location, :unless => :devise_controller?
-
   def current_ability
     if jobseeker_signed_in? 
       @current_ability ||= JobseekerAbility.new(current_jobseeker)
@@ -19,28 +13,25 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  
   def after_sign_in_path_for(resource)
-    resource.is_a?(Employer) ? applications_path  : session[:previous_url]
-    resource.is_a?(Jobseeker) ? jobs_path  : session[:previous_url] 
+    stored_location_for(resource) ||
+      if resource.is_a?(Employer)
+        applications_path
+      elsif resource.is_a?(Jobseeker)
+        jobs_path
+      else
+        super
+      end
   end
-
-  # def store_location
-  #   # store last url - this is needed for post-login redirect to whatever the user last visited.
-  #   return unless request.get? 
-  #   if (request.path != "/jobseekers/sign_in" &&
-  #       request.path != "/jobseekers/sign_up" &&
-  #       request.path != "/jobseekers/password/new" &&
-  #       request.path != "/jobseekers/password/edit" &&
-  #       request.path != "/jobseekers/confirmation" &&
-  #       request.path != "/jobseekers/sign_out" &&
-  #       !request.xhr?) # don't store ajax calls
-  #     session[:previous_url] = request.fullpath 
-  #   end
-  # end
-
-  private  
-    def after_sign_out_path_for(resource_or_scope)
+  
+  private
+   def layout_by_resource
+    if devise_controller? && resource_name == :employer
+      "dashboard"
+    end
+  end
+  
+  def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :jobseeker
       jobs_path
     elsif resource_or_scope == :employer
