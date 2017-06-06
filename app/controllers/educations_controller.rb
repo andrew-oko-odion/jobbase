6cb37506c5250bc2ad04892911217928fe8e6d27
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-06-06 16:04:58 +0100

    Make Education resource respond_to json data for its CRUD actions

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,46 +1,70 @@
 class EducationsController < ApplicationController
+  before_action :authenticate_jobseeker!
+  layout 'jobsinternships_layout'
   
   def index
     @educations  = Education.where(jobseeker_id: current_jobseeker.id)
+    respond_to do |format|
+      format.html
+      format.json {render json: @educations, status: :ok }
+    end
   end
 
   def show
     @education = Education.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @education, status: :ok, location: @education }
+    end
   end
 
   def new
+    @education_type = EducationType.all
     @education = Education.new
   end
 
   def create
-   if  @education = Education.new(education_params)
-     @education.save
-     flash[:notice] = 'Added Successfully'
-     redirect_to @education
-   else
-     render 'new'
-   end
+    respond_to do |format|
+      if @education = Education.new(education_params)
+        @education.save
+        format.html { redirect_to @education, notice: 'Added Successfully' }
+        format.json { render json: @education, status: :ok, location: @education }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @education.errors, status: :unprocessable_entity }
+      end
+    end
   end
   
   def edit
+    @education_type = EducationType.all
     @education = Education.find(params[:id])
   end
 
   def update
-   # @education = Education.find(params[:id])
-    if @education.update_attributes(education_params)
-       flash[:notice] = 'Updated Successfully'
-      redirect_to @education
-    else
-      render 'edit'
+    respond_to do |format|
+      if @education.update_attributes(education_params)
+        format.html {redirect_to @education, notice: 'Updated Successfully.' }
+        format.js
+        format.json { render json: @education, status: :ok, location: @education}
+      else
+        format.html {render action: 'edit'}
+        format.json {render json: @education.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
     @education = Education.find(params[:id])
-    @education.destroy
-     flash[:notice] = 'Deleted Successfully'
-    redirect_to educations_path
+    respond_to do |format|
+      if @education.destroy 
+        format.html { redirect_to educations_path, notice: 'Deleted Successfully'}
+        format.json { render json: "Deleted Successfully", status: :ok }
+      else
+        format.html { redirect_to educations_path, notice: "Error Unable to Delete" }
+        format.json {render json: @education.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
