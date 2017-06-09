80be5194c51409d5eda77164c05355cb68ac283a
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-06-09 08:55:48 +0100

    Update UI views and actions for Jobs_controller

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,23 +1,14 @@
 
 class Job < ApplicationRecord
-  extend FriendlyId  
+  extend FriendlyId
+  searchkick word_start: [:title]
   friendly_id :slug_jobs, use: :slugged
-  searchkick word_start:  [:title]
+
   belongs_to :job_type
   belongs_to :job_category
   belongs_to :employer
   has_many :application
   has_many :payment
-
- # scope :next, lambda {|id| where("id > ?",id).order("id ASC") }
- # scope :previous, lambda {|id| where("id < ?",id).order("id DESC") }
-
-  # scope :next, -> { where(job_type: 1, hide: false).where("id > ?", id).order("id ASC") }
-  
-  # def next
-  #   # Job.where(job_type: 1, hide: false).where("id > ?", id).order("id DESC").first || Job.first
-  #   Job.where(job_type: 1, hide: false).where("id > ?", id).order("id ASC").first
-  # end
 
   def next(job)
     Job.where(job_type: 1, hide: false).where('id < ?', job.id).last
