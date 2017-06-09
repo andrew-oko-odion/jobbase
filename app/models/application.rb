b062d173a71ca6040c9a7d9670229d29acaee697
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-06-09 09:23:21 +0100

    Add search action to application

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,7 +1,15 @@
 class Application < ApplicationRecord
   validates :id, uniqueness: { scope: [:job_id, :jobseeker_id], message: "you already applied for this job" }, on: :create
+  searchkick #word_start: [:title]
+  
   belongs_to :jobseeker
   belongs_to :job
   belongs_to :sort_order
   belongs_to :employer
+
+  # def search_data
+  #   {
+  #     title: jobseeker.fullname,
+  #   }
+  # end
 end
