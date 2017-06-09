2ed86d22b6cac17fe3dee5bd61796ea5eb0aeedc
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-06-09 09:00:06 +0100

    Add layout to devise jobseekers templates

--- app/controllers/jobseekers/registrations_controller.rb
+++ app/controllers/jobseekers/registrations_controller.rb
@@ -1,12 +1,18 @@
 class Jobseekers::RegistrationsController < Devise::RegistrationsController
+  #include Accessible
+  #before_action :check_user
+  layout 'jobsinternships_layout'
   
- before_action :configure_sign_up_params, only: [:create]
- before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
+  # GET /resource/index
+  def index
+  end
+ 
   # GET /resource/sign_up
    def new
      super
-     @education = Education.new
    end
 
   # POST /resource
@@ -39,7 +45,10 @@ class Jobseekers::RegistrationsController < Devise::RegistrationsController
    end
 
    protected
-
+   
+   def update_resource(resource, params)
+     resource.update_without_password(params)
+   end
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
      devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,48 +1,7 @@
 class  WelcomeController < ApplicationController
-# load_and_authorize_resource :only => [:index, :show] 
-  layout 'welcome_layout'
+  # layout 'welcome_layout'
   #check_authorization
   #load_and_authorize_resource 
-  
-  def autocomplete
-    render json: Job.search(params[:query], autocomplete: true, limit: 10).map  do |j|
-      { title: j.title, value: j.id }
-    end
-  end
-  
   def index
-    if params[:query].present?
-      @jobs = Job.search(params[:query], page: params[:page]) #where(job_type: 1, hide: false).paginate(:page => params[:page], :per_page => 6)
-    end
-  end
-
-  def show
-    @job = Job.find(params[:id])
-    @apply = Application.new
-  end
-  
-
-  def create
-    @apply = Application.new(applications_params)    
-    respond_to do |format|
-      if jobseeker_signed_in? 
-        if @apply.save
-          format.html { redirect_to welcome_index_path, notice: 'Application sent Successfully.' }
-          format.js
-        end
-      else
-        format.html { redirect_to new_jobseeker_session_path, error: 'Please Login to apply' }
-      end
-    end
-  end
-  
-  private
-  def job_params
-    params.require(:job).permit(:title, :job_type_id, :job_category_id, :description, :application_email, :location, :company_name, :closing_date, :salary, :employer_id)
-  end
-
-  def applications_params
-    params.require(:apply).permit(:jobseeker_id, :job_id, :employer_id, :sort_order_id)
-  end
-  
+  end  
 end
