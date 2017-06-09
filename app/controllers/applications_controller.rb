26b628de553de9eb5d8f2aca0d6e17c85b6d1e04
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-06-09 08:28:22 +0100

    create action to allow employer view candidate profile

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,39 +1,56 @@
-
-# Handle Applications for the jobseeker while it also handles Employers selection of candidate
 class ApplicationsController < ApplicationController
+  before_action :authenticate_employer!
   layout 'dashboard'
   def index
-    # List of submited applications for a particular Job.
-    @all = Application.where(:employer_id = current_employer.id)
+    @total_jobs = Job.where(employer_id: current_employer.id, active: :true)
+    @applications_sort = Application.where(job_id: params[:id], sort_order_id: params[:sort_order_id])
+    @candidates = Application.where(job_id: params[:job_id], employer_id: current_employer.id).order('id DESC').plant(params[:page]).per(3)
+    @accepted = Application.where(job_id: params[:job_id], employer_id: current_employer.id, sort_order_id: 2).order('id DESC').plant(params[:page]).per(3)
+    @interview = Application.where(job_id: params[:job_id], employer_id: current_employer.id, sort_order_id: 3).order('id DESC').plant(params[:page]).per(3)
+    @rejected = Application.where(job_id: params[:job_id], employer_id: current_employer.id, sort_order_id: 1).order('id DESC').plant(params[:page]).per(3)
+  end
+  
+  def show
+    # @profile = Application.where({job_id: params[:job_id], jobseeker_id: params[:jobseeker_id]}).first
+    @profile = Application.find(params[:id])
+    @candidate_application = Application.find(params[:id])
   end
 
-  def show
-    # shows a Jobseekers profile I have not decided whether it should be a modal or not 
-    # @profile = @application.find("where: Job.employer_id.jobseeker.id")
+  def search
+    @search_jobseeker = Application.search(params[:query]) #where: {limit: 6, employer_id: current_employer.id} )
   end
 
   def new
-    #@applications = Application.new
+    @job = Job.new
   end
 
-  def create
-    if @applications = Application.new(applications_params) &&  current_jobseeker 
-      @applications.save
-      flash[:notice] = 'Application sent Successfully'
-      redirect_to welcome_index_path
-    else
-      flash[:notice] = 'Please Login'
-      redirect_to new_jobseeker_session_path 
+  def update
+    @candidate_application = Application.find(params[:id])
+    @candidate_application.update_attributes(sort_params)
+    respond_to do |f|
+      f.js 
     end
-   # render plain: params[:applications].inspect
   end
 
-  private 
-  def applications_params
-    params.require(:applications).permit(:jobseeker_id, :job_id)
+ 
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      flash[:notice] = "Job Added to Cart"
+      session[:job_id] = @job.id
+      redirect_to new_payment_path
+    else
+      flash[:alert] = "Job Post not Successfull"
+      render :new
+    end
   end
 
   def job_params
-    params.require(:job).permit(:title, :job_type_id, :job_category_id, :description, :application_email, :location, :company_name, :closing_date, :salary, :employer_id)
+    params.require(:job).permit(:title, :job_type_id, :job_category_id, :description, :application_email, :location, :company_name, :closing_date, :salary, :employer_id, :card_no, :cvv, :expiry_year, :expiry_month, :amount, :firstname, :lastname, :phonenumber, :email, :recipient_bank, :recipent_account_number,  :apiKey, :redirecturl, :medium, :authoriztion, :tags)
   end
+
+  def sort_params
+    params.require(:application).permit(:sort_order_id)
+  end
+
 end
