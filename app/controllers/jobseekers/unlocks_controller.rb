52dc2d506d31645210922f906c60edd9ff08f8b6
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-07-09 23:14:10 +0100

    Fix Typo

--- app/controllers/jobseekers/unlocks_controller.rb
+++ app/controllers/jobseekers/unlocks_controller.rb
@@ -1,16 +1,16 @@
 class Jobseekers::UnlocksController < Devise::UnlocksController
   
-   GET /resource/unlock/new
+   # GET /resource/unlock/new
    def new
      super
    end
 
-   POST /resource/unlock
+   # POST /resource/unlock
    def create
      super
    end
 
-   GET /resource/unlock?unlock_token=abcdef
+   # GET /resource/unlock?unlock_token=abcdef
    def show
      super
    end
